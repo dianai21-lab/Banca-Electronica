1d138da35782b6d78e37c36dbb5973e68cdc955e
dianai21-lab <dianaiperezp@outlook.com>
2024-07-18 16:48:36 -0500

    Delete bancaElectronica directory

--- bancaElectronica/personal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.ingreso, name="ingreso"),
-    path("Activar/<int:usuario>/", views.activacion, name="activacion"),
-]
